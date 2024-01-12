5969b2f811891cfc38f16915186852ca7d260f8f
yanxiaodi <yan_xiaodi@hotmail.com>
2024-01-13 12:22:21 +1300

    update

--- convert.py
+++ convert.py
@@ -1,17 +1,20 @@
 import os
 import eyed3
+import yaml
 
-# 从 audio 文件夹中读取 mp3 文件并返回一个包含 title 和 comments 的列表
 def get_audio_files():
     audio_files = []
     for file in os.listdir('audio'):
         if file.endswith('.mp3'):
             audio_file = eyed3.load(os.path.join('audio', file))
-            audio_files.append({
+            comments = [comment.text for comment in audio_file.tag.comments]
+            comments_str = ', '.join(comments)
+            audio_info = {
                 'title': audio_file.tag.title,
-                'comments': audio_file.tag.comments
-            })
-    return audio_files
+                'comments': comments_str
+            }
+            audio_files.append(audio_info)
+    return yaml.dump(audio_files)
 
 # 调用函数并打印返回的列表
 print(get_audio_files())
